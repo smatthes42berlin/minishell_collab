c1d7b575ead63b29d3c39e59d32fed850e9d6587
ELREKO <rene.kost@gmail.com>
2024-02-07 13:56:42 +0100

    update sh. script

--- create_valgrind_logging.sh
+++ create_valgrind_logging.sh
@@ -6,6 +6,20 @@ LOG_DIR="valgrind_logs"
 # Das zu startende Programm (ersetzen Sie ./deinProgramm mit dem tatsächlichen Pfad zu Ihrem Programm).
 PROGRAM="./minishell"
 
+make
+MAKE_STATUS=$?
+
+# Überprüfen, ob make erfolgreich war
+if [ $MAKE_STATUS -ne 0 ]; then
+ echo -e "\033[1m\033[31m \n\n\nMake ist fehlgeschlagen, Skript wird beendet.\033[0m \n"
+    exit 1
+fi
+
+clear
+
+
+echo -e "\033[32m \n\n Make file Succescc \033[0m \n\n"
+
 # Überprüfen, ob der Log-Ordner existiert. Wenn ja, leere ihn. Wenn nein, erstelle ihn.
 if [ -d "$LOG_DIR" ]; then
     echo "Leere vorhandenen Log-Ordner: $LOG_DIR"
