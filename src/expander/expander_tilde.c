1eedeb8676ce279902678081e7059e732c808381
Steffen Matthes <smatthes@student.42berlin.de>
2024-03-06 17:02:50 +0100

    added ~ expansion when something starting with ~/

--- src/expander/expander_tilde.c
+++ src/expander/expander_tilde.c
@@ -1,5 +1,7 @@
 #include "minishell.h"
 
+static int	replace_tilde_path(t_token *cur_token, t_main_data *main_data);
+
 int	expander_tilde(t_token *cur_token, t_main_data *main_data)
 {
 	if (ft_strcmp("~", cur_token->value) == 0)
@@ -9,5 +11,30 @@ int	expander_tilde(t_token *cur_token, t_main_data *main_data)
 		if (!cur_token->value)
 			return (1);
 	}
+	if (ft_strncmp("~/", cur_token->value, 2) == 0)
+	{
+		if (replace_tilde_path(cur_token, main_data))
+			return (1);
+	}
+	return (0);
+}
+
+static int	replace_tilde_path(t_token *cur_token, t_main_data *main_data)
+{
+	char	*tmp;
+	char	*home;
+	int		state;
+
+	home = env_get_var(main_data, "HOME");
+	if (!cur_token->value)
+		return (1);
+	state = ins_replace_str_after_index(&tmp, (t_ins_repl_str){cur_token->value,
+			"~", home, 0});
+	free_char_variadic_code(0, 2, cur_token->value, home);
+	cur_token->value = tmp;
+	if (state)
+		return (throw_error_custom((t_error_ms){errno, EPART_EXPANDER,
+				EFUNC_MALLOC,
+				"replacing env var in token val in tilde expansion"}));
 	return (0);
 }
